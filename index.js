0c67edd6a3b0f21fbb670ea5040bb905e3648587
kosteg <kostyrkoevgeniy@yandex.ru>
2022-09-24 10:40:37 +0300

    добавил файлы первого занятия

---
+++ index.js
@@ -0,0 +1,20 @@
+ console.log("Hello world!");
+
+ const ourPlanetName = 'Земля';
+
+ console.log(ourPlanetName);
+
+ let currentTime = 18.00;
+ let countOfArticles = 0;
+ let isCash = true;
+ let firstName = "Evgeniy";
+ let surName = "Petrovich"; 
+ let lastName = "Ivanov";
+
+ console.log(currentTime);
+ console.log(countOfArticles);
+ console.log(isCash);
+ console.log(firstName);
+ console.log(surName);
+ console.log(lastName);
+ 
