7cdce7e031a30a55d21894dd68cb6e4c6777821d
kosteg <kostyrkoevgeniy@yandex.ru>
2022-09-24 12:48:55 +0300

    типы данных

--- index.js
+++ index.js
@@ -1,20 +1,67 @@
-console.log("Hello world!");
+// console.log("Hello world!");
 
-const ourPlanetName = "Земля";
+// const ourPlanetName = "Земля";
 
-console.log(ourPlanetName);
+// console.log(ourPlanetName);
 
-let currentTime = 18.0;
+// let currentTime = 18.0;
 
-let countOfArticles = 0;
-let isCash = true;
-let firstName = "Evgeniy";
-let surName = "Petrovich";
-let lastName = "Ivanov";
+// let countOfArticles = 0;
+// let isCash = true;
+// let firstName = "Evgeniy";
+// let surName = "Petrovich";
+// let lastName = "Ivanov";
 
-console.log(currentTime);
-console.log(countOfArticles);
-console.log(isCash);
-console.log(firstName);
-console.log(surName);
-console.log(lastName);
+// // console.log(currentTime);
+// // console.log(countOfArticles);
+// // console.log(isCash);
+// // console.log(firstName);
+// // console.log(surName);
+// // console.log(lastName);
+
+// const nameFirst = "Sobaka";
+
+// const age = 15;
+
+// const userDataName = `имя пользователя ${nameFirst}, возраст ${age}`;
+
+// const userDataNew = "имя пользователя ${nameFirst}, возраст ${age}";
+// console.log(userDataName);
+
+// //Комментарии однострочный
+
+// const isVisble = true;
+
+// /* многострочный комментарий
+// const isVisbleFalse = false;
+// */
+
+// // null - ничего
+// let userName = null;
+
+// //undefined - неопределенность, не присвоенно значение
+// let townName;
+
+// console.log(townName);
+
+// townName = "Moscow";
+// console.log(townName);
+
+/*
+обьекты являются ссылочными типами данных 
+остальные примитивными типами данных 
+*/
+
+const userData = {
+  name: "Ivan",
+  age: 34,
+};
+
+console.log(userData);
+
+console.log(typeof userData.age);
+
+console.log(typeof userData)
+
+const num = 1234567890123456789n
+console.log(num)
