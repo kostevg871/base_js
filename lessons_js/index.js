500f9f1c03806743960923711b1cab9c69652c9b
kosteg <kostyrkoevgeniy@yandex.ru>
2022-10-14 23:47:42 +0300

    hw ls 3

--- lessons_js/index.js
+++ lessons_js/index.js
@@ -70,39 +70,66 @@
 
 // console.log("ivan" / 4);
 
-// console.log(typeof Number("123zxc") === "number");
+console.log(typeof Number("123zxc") === "number");
 
-// const name = "Alex";
-// const nameToNumber = Number(name);
-// console.log(nameToNumber);
+const name = "Alex";
+const nameToNumber = Number(name);
+console.log(nameToNumber);
 
-// const age = 26;
-// const ageToString = String(age);
-// console.log(typeof ageToString);
+const age = 26;
+const ageToString = String(age);
+console.log(typeof ageToString);
 
-// const nullValue = null;
-// console.log(Number(nullValue));
+const nullValue = null;
+console.log(Number(nullValue));
 
-// console.log(Boolean(""));
-// console.log(Boolean(0));
-// console.log(Boolean({}));
+console.log(Boolean(""));
+console.log(Boolean(0));
+console.log(Boolean({}));
 
-// console.log(1 + "000");
+console.log(1 + "000");
 
-// console.log(2 == "2");
+console.log(2 == "2");
 
-// console.log(3 == "3");
-// console.log(1 === "3");
-
+console.log(3 == "3");
+console.log(1 === "3");
 
 // practice
 console.log("ананас" > "яблоко");
 console.log("2" > "12");
-console.log( null == "null");
+console.log(null == "null");
 console.log("" + 1 + 0);
 console.log("$" + 5 + 5);
 console.log("4px" - 2);
-console.log(7/0);
+console.log(7 / 0);
 console.log(null + 1);
 console.log(undefined + 1);
 console.log("\t\n" - 2);
+
+const array = ["Shrek", "Shrek 2", "Shrek 3", "Shrek 4"];
+console.log(array[0]);
+array.push("Shrek forever");
+console.log(array[array.length - 1]);
+
+//home lessons 3
+console.log("кот" > "код"); //false - true
+console.log("2" + 2 * "2"); //24 +
+console.log(undefined == null); // true +
+console.log(undefined != null); // false +
+console.log(null == 0); // false +
+console.log(2 > "3"); //false +
+console.log(null - false + true); // 1  +
+console.log(1 / "1"); // 1  +
+console.log("2" * "3"); // 6 +
+console.log(4 + 5 + "O"); // "9o" +
+console.log("1" + 4 + 5); //145 +
+console.log("4" - 2); // 2 +
+console.log("4" - "4x"); //NaN +
+console.log("23" == 23); // true +
+console.log(null == false); //false +
+console.log(" -4 " / 0 + 1); // -infinity +
+console.log(null + 1); //1 +
+console.log(undefined + null); // 1 - Nan
+console.log(1 === "1"); //true - false
+console.log("2" > 10); // false +
+console.log(NaN == undefined); // true - false
