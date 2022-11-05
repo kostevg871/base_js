b93b50d8282cdccc3cceed7f73a6545c1cd7d8bd
kosteg <kostyrkoevgeniy@yandex.ru>
2022-11-05 11:37:18 +0300

    lessons 8 for-of, for-in, rest-operator

--- lessons_js/lessons8.js
+++ lessons_js/lessons8.js
@@ -1,10 +1,10 @@
-const number = 1;
+// const number = 1;
 
-const increment = (a) => {
-  //   console.log(a, "before");
-  a += 1;
-  //   console.log(a, "after");
-};
+// const increment = (a) => {
+//   console.log(a, "before");
+//   a += 1;
+//   console.log(a, "after");
+// };
 
 // console.log(number, "one");
 // increment(number);
@@ -48,12 +48,12 @@ const increment = (a) => {
 // console.log(arr);
 // console.log(arr2);
 
-const arr = [1, 2, 3, 4];
+// const arr = [1, 2, 3, 4];
 
-const arrPush = (arr) => {
-  const arrNew = [...arr];
-  arrNew.push(5);
-};
+// const arrPush = (arr) => {
+//   const arrNew = [...arr];
+//   arrNew.push(5);
+// };
 
 // console.log(arr, "One");
 // arrPush(arr);
@@ -62,10 +62,46 @@ const arrPush = (arr) => {
 // console.log(JSON.stringify(user), user);
 
 // 3 JSON.parse(JSON.stringify())
-const user = { name: "Alex", age: 18 };
-const user2 = JSON.parse(JSON.stringify(user));
-user.name = "Ivan";
-user2.age = 20;
+// const user = { name: "Alex", age: 18 };
+// const user2 = JSON.parse(JSON.stringify(user));
+// user.name = "Ivan";
+// user2.age = 20;
 
-console.log(user);
-console.log(user2);
+// console.log(user);
+// console.log(user2);
+
+// 4 rest оператор
+// const infinitySum = (... args) => {
+//     return args.reduce((prev, curr) => prev + curr, 0)
+// }
+
+// console.log(infinitySum(1,23, 9));
+
+// const user = { name: "Alex", age: "18", city: "London" };
+
+// const userName = user.name;
+// const userAge = user.age;
+// console.log(userName, "UserName");
+
+// const { name, ...restParams } = user;
+// console.log(name, "name");
+// console.log(restParams, "restParams");
+
+// for of
+// const arraySum = (arr) => {
+//   let result = 0;
+//   for (let value of arr) {
+//     result += value;
+//   }
+//   return result;
+// };
+
+// console.log(arraySum([1, 2, 3, 4, 5, 6, 7, 8]));
+
+// for in переход по ключам
+// const arr = ['Ivan', 'Alex', 'Tony']
+// const obj = {name: 'Alex', age:18}
+
+// for(let value in obj){
+//     console.log(obj[value])
+// }
