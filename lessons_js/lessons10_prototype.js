e4948a99af1211d03fac9e950204db3e087fa0fa
kosteg <kostyrkoevgeniy@yandex.ru>
2022-11-21 21:38:10 +0300

    lessons 10 end

--- lessons_js/lessons10_prototype.js
+++ lessons_js/lessons10_prototype.js
@@ -44,10 +44,85 @@ function CreateUser(name, age) {
 function CreateUserExample(userName, userAge) {
   this.name = userName;
   this.age = userAge;
+
+  this.sayHi = function () {
+    console.log(`Hello, my name is ${this.name}`);
+  };
 }
+
+CreateUserExample.prototype.sayAge = function () {
+  console.log(`Hello, my age is ${this.age}`);
+};
 
 let firstUser = new CreateUserExample("Ivan", 27);
 let secondUser = new CreateUserExample("Jeka", 36);
 
 // console.log(firstUser, secondUser);
 
+// firstUser.sayHi()
+
+// secondUser.sayAge();
+
+function CreateSend(from, to) {
+  this.from = from;
+  this.to = to;
+
+  this.show = function () {
+    console.log(`from ${this.from} to ${this.to}`);
+  };
+}
+
+let firstSend = new CreateSend("Moscow", "NN");
+let secondSend = new CreateSend("NN", "Paris");
+
+// console.log(firstSend);
+// secondSend.show();
+
+CreateSend.prototype.fromChange = function (from) {
+  this.from = from;
+};
+
+// console.log(firstSend.from);
+// firstSend.fromChange("Argentina");
+// console.log(firstSend.from);
+
+// Классы
+
+class Parent {
+  constructor(name) {
+    this.name = name;
+  }
+
+  sayHiClass() {
+    console.log("My name" + this.name);
+  }
+}
+
+class People {
+  constructor(name, surname) {
+    this.name = name;
+    this.surname = surname;
+  }
+
+  getFullName() {
+    return `${this.name} ${this.surname}`;
+  }
+}
+
+const firstPeople = new People("Alex", "Ivanov");
+// console.log(firstPeople.getFullName());
+
+class Worker extends People {
+  constructor(name, surname, rate, day) {
+    super(name, surname);
+    this.rate = rate;
+    this.day = day;
+  }
+  getSallary() {
+    return `${this.getFullName()} получил ${this.rate * this.day}`;
+  }
+}
+
+const firstWorker = new Worker("Alex", "Petrov", 500, 20);
+console.log(firstWorker.getFullName());
+console.log(firstWorker.getSallary());
