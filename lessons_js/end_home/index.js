fa315bb323ca18836b6b9f58fdea8f426b41f8b8
kosteg <kostyrkoevgeniy@yandex.ru>
2022-12-01 19:03:44 +0300

    home work, end base course

---
+++ lessons_js/end_home/index.js
@@ -0,0 +1,9 @@
+// wrapper
+
+let wrapper = document.createElement("wrapper");
+document.body.prepend(wrapper);
+wrapper.className = "wrapper";
+
+let header = document.createElement("header");
+header.className = "header";
+wrapper.prepend(header);
