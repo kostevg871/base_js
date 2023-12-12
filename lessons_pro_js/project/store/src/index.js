d3f0bc8aa4925eb1735b8b25c2839ceac9b4391f
Evgeniy <kostyrkoevgeniy@yandex.ru>
2023-12-12 20:42:26 +0300

    styles

---
+++ lessons_pro_js/project/store/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import "./styles/index.css";
+
+import App from "./components/App/App";
+
+createRoot(document.getElementById("root")).render(<App />);
