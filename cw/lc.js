fcb5a157010c4cc355d6c0c6cb46d5f846f66a60
Evgeniy <kostyrkoevgeniy@yandex.ru>
2023-12-20 23:10:42 +0300

    7 Easy lc

--- cw/lc.js
+++ cw/lc.js
@@ -109,3 +109,122 @@ var expect = function (val) {
 
 //console.log(expect(5).toBe(null));
 //console.log(expect(5).notToBe(null));
+
+//5 Easy
+/*Спроектируйте Calculatorкласс. Класс должен обеспечивать математические операции сложения,
+вычитания, умножения, деления и возведения в степень. Он также должен позволять выполнять последовательные операции
+с использованием цепочки методов. Конструктор Calculatorкласса должен принимать число, которое служит начальным значением result.
+
+Ваш Calculator класс должен иметь следующие методы:
+
+add— Этот метод добавляет заданное число valueк  resultи возвращает обновленное значение Calculator.
+subtract- Этот метод вычитает заданное число value из  resultи возвращает обновленное значение Calculator.
+multiply— Этот метод умножает result  на заданное число valueи возвращает обновленное значение Calculator.
+divide- Этот метод делит resultна заданное число valueи возвращает обновленное значение Calculator. 
+Если переданное значение равно , должна быть выдана 0ошибка ."Division by zero is not allowed"
+power- Этот метод возводит число  resultв степень заданного числа valueи возвращает обновленное значение Calculator.
+getResult- Этот метод возвращает result.
+Решения в пределах   фактического результата считаются правильными.10-5 */
+
+class Calculator {
+  constructor(value) {
+    //this.value = value;
+    this.result = value;
+  }
+
+  add(value) {
+    this.result = this.result + value;
+    // можно использовать this
+    return this;
+  }
+
+  subtract(value) {
+    this.result = this.result - value;
+    return new Calculator(this.result);
+  }
+
+  multiply(value) {
+    this.result = this.result * value;
+    return new Calculator(this.result);
+  }
+
+  divide(value) {
+    if (value === 0) {
+      throw new Error("Division by zero is not allowed");
+    } else {
+      this.result = this.result / value;
+      return new Calculator(this.result);
+    }
+  }
+
+  power(value) {
+    this.result = this.result ** value;
+    return new Calculator(this.result);
+  }
+
+  getResult() {
+    return this.result;
+  }
+}
+
+//console.log(new Calculator(20).divide(0).getResult());
+
+//6 Easy
+/*Учитывая функцию fn, массив аргументов  argsи интервал времени t, верните функцию отмены cancelFn.
+
+Функцию fn следует вызывать args немедленно, 
+а затем вызывать снова каждые  t миллисекунды, пока  cancelFn она не будет вызвана в cancelTimeMsмс. */
+
+var cancellable = function (fn, args, t) {
+  fn(...args);
+
+  let timer = setInterval(() => fn(...args), t);
+
+  let cancelFn = () => clearInterval(timer);
+  return cancelFn;
+};
+
+//const result = [];
+
+//const fn = (x) => x * 2;
+//const args = [4],
+//  t = 35,
+//  cancelTimeMs = 190;
+
+//const start = performance.now();
+
+//const log = (...argsArr) => {
+//  const diff = Math.floor(performance.now() - start);
+//  result.push({ time: diff, returned: fn(...argsArr) });
+//};
+
+//const cancel = cancellable(log, args, t);
+
+//setTimeout(cancel, cancelTimeMs);
+
+//setTimeout(() => {
+//  console.log(result);
+//}, cancelTimeMs + t + 15);
+
+//7 Easy
+
+/*
+Учитывая объект или массив, верните значение, если он пуст.
+
+Пустой объект не содержит пар ключ-значение.
+Пустой массив не содержит элементов.
+Вы можете предположить, что объект или массив являются выходными данными  JSON.parse.
+*/
+
+var isEmpty = function (obj) {
+  //  if (Object.keys(obj).length === 0) {
+  //    return true;
+  //  } else {
+  //    return false;
+  //  }
+
+  for (const _ in obj) return true;
+  return false;
+};
+
+//console.log(isEmpty([]));
