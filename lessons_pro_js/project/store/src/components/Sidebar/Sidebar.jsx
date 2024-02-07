d4b66f80659cdb25c9b6d6d0dc573e5fd2d21b4e
Evgeniy <kostyrkoevgeniy@yandex.ru>
2024-02-07 20:07:45 +0300

    footer+sidebar

--- lessons_pro_js/project/store/src/components/Sidebar/Sidebar.jsx
+++ lessons_pro_js/project/store/src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 
-import
+import styles from "../../styles/Sidebar.module.css";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <section className={styles.sidebar}>
+      <div className={styles.title}>CATEGORIES</div>
+      <nav>
+        <ul className={styles.menu}>
+          <li>
+            <NavLink to={`/categories/${1}`}>Link</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.footer}>
+        <a href="/help" targer="_blank" className={styles.link}>
+          Help
+        </a>
+        <a
+          href="/terms"
+          targer="_blank"
+          className={styles.link}
+          style={{ textDecoration: "underline" }}
+        >
+          Terms and Conditions
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default Sidebar;
