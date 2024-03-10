97bcfc48fb380856b5814638e790b5ff79c36a01
Evgeniy <kostyrkoevgeniy@yandex.ru>
2024-03-10 19:33:35 +0300

    update user

--- lessons_pro_js/project/store/src/features/user/userSlice.js
+++ lessons_pro_js/project/store/src/features/user/userSlice.js
@@ -34,6 +34,19 @@ export const loginUser = createAsyncThunk(
   }
 );
 
+export const updateUser = createAsyncThunk(
+  "users/updateUser",
+  async (payload, thunkAPI) => {
+    try {
+      const res = await axios.put(`${BASE_URL}/users/${payload.id}`, payload);
+      return res.data;
+    } catch (err) {
+      console.log(err);
+      return thunkAPI.rejectWithValue(err);
+    }
+  }
+);
+
 const addCurrentUser = (state, { payload }) => {
   state.currentUser = payload;
   state.isLoading = false;
@@ -74,6 +87,7 @@ const userSlice = createSlice({
   extraReducers: (builder) => {
     builder.addCase(createUser.fulfilled, addCurrentUser);
     builder.addCase(loginUser.fulfilled, addCurrentUser);
+    builder.addCase(updateUser.fulfilled, addCurrentUser);
   },
 });
 
