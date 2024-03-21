de2b01b2218a811f086cb969c0ff2fb3067493ef
Evgeniy <kostyrkoevgeniy@yandex.ru>
2024-03-21 20:43:27 +0300

    refactoring repo, project to main

--- lessons_pro_js/project/store/src/features/categories/selectors.js
+++
@@ -1,7 +0,0 @@
-import { createSelector } from "@reduxjs/toolkit";
-
-const getCategoriesSlice = (state) => state.categories;
-
-export const categoriesSelect = createSelector(getCategoriesSlice, (data) => {
-  return data.list;
-});
